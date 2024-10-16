5cadc2adf2808a7a84ef1dfc41524bb0fac285a9
Florin Pinta <xttrust@live.com>
2024-10-16 18:35:07 +0000

    feat: Added bag context delivery logic

--- core/settings.py
+++ core/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -166,3 +167,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Delivery
+FREE_DELIVERY_THRESHHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
