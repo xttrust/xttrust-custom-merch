cf779e9217a207bc9d09df44889e80ea93a0f126
Florin Pinta <xttrust@live.com>
2024-10-20 18:25:22 +0000

    fix: Fixed reviews, users are now able to delete only reviews of theyr own.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,5 @@
+# views.py in the reviews app
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -5,14 +7,14 @@ from products.models import Product
 from .models import Review
 from .forms import ReviewForm
 
-
 def product_reviews(request, product_id):
+    """ View to display all reviews for a product """
     product = get_object_or_404(Product, pk=product_id)
     reviews = Review.objects.filter(product=product)
-    user_review = None
     star_range = range(1, 6)
 
     # Check if the logged-in user has a review for this product
+    user_review = None
     if request.user.is_authenticated:
         user_review = reviews.filter(user=request.user).first()
 
@@ -35,7 +37,7 @@ def add_review(request, product_id):
     existing_review = Review.objects.filter(product=product, user=request.user).first()
     if existing_review:
         messages.error(request, 'You have already reviewed this product.')
-        return redirect('product_detail', product_id=product.id)
+        return redirect('product_reviews', product_id=product.id)
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -45,7 +47,7 @@ def add_review(request, product_id):
             review.user = request.user
             review.save()
             messages.success(request, 'Your review has been added!')
-            return redirect('product_detail', product_id=product.id)
+            return redirect('product_reviews', product_id=product.id)
         else:
             messages.error(request, 'Please correct the errors below.')
     else:
@@ -62,13 +64,13 @@ def delete_review(request, review_id):
     # Ensure the logged-in user is the owner of the review
     if review.user != request.user:
         messages.error(request, 'You are not authorized to delete this review.')
-        return redirect('product_detail', product_id=review.product.id)
+        return redirect('product_reviews', product_id=review.product.id)
 
     # If POST request, delete the review
     if request.method == 'POST':
         review.delete()
         messages.success(request, 'Your review has been deleted.')
-        return redirect('product_detail', product_id=review.product.id)
+        return redirect('product_reviews', product_id=review.product.id)
 
     # If GET request, show confirmation page
     return render(request, 'reviews/confirm_delete.html', {'review': review})
